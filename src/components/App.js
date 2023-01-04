fe71c47f840f523462b12caf3c110c043e025b07
Sajan Jain <sajan.jain@ocean.ibm.com>
2023-01-04 16:23:21 +0530

    New Draw Card Component Added along with Redux flow

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { startGame, cancelGame } from '../actions/settings';
 import { fetchNewDeck } from '../actions/deck';
 import fetchStates from '../reducers/fetchStates';
 import Instructions from './Instructions';
+import DrawCard from './DrawCard';
 
 class App extends Component {
   startGame = () => {
@@ -27,6 +28,8 @@ class App extends Component {
             <div>
               <h3>The game is on!</h3>
               <br />
+              <DrawCard />
+              <hr />
               <button onClick={this.props.cancelGame}>Cancel Game</button>
             </div>
           ) : (
